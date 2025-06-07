e7fcd6c1ad39ffa627fcbc2b8f49dce7003d2575
Chowdhury <md.nasif008@gmail.com>
2025-06-08 01:35:04 +0200

    number count added

--- class and object/creating_class_and_object.py
+++ class and object/creating_class_and_object.py
@@ -1,12 +1,16 @@
 class Patient():
     status = 'patient'
+    total_patients = 0
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
         self.conditions = []
 
+        Patient.total_patients += 1
+
     def get_details(self):
-        print(f'{self.name} is {self.age} years old.' \
+        print(f'{Patient.total_patients}. {self.name} is {self.age} years old.' \
               f' Current condition is : {self.conditions}')
            
     def add_info(self, information):
@@ -15,3 +19,7 @@ class Patient():
 steve = Patient('Steven Smith', 52)
 steve.add_info('patient treated to be ear infection.')
 steve.get_details()
+
+huge = Patient('Carlos Huge', 60)
+huge.add_info('patient treated to be heart diseases.')
+huge.get_details()
